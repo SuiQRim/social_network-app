a20057b8e4065c614a0669a18d6ee4890cb869be
Roman Samadi <romikkimor123@gmail.com>
2022-11-15 04:19:09 +0300

    Добавил кнопку из 52 урока

--- src/Assets/Content/Users/Users.jsx
+++ src/Assets/Content/Users/Users.jsx
@@ -3,16 +3,20 @@ import User from './User/User';
 import style from './Users.module.css'
 
 function Users(props) {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(responce => {
-            props.setUsers(responce.data.items);
-        })
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(responce => {
+                props.setUsers(responce.data.items);
+            })
+        }
     }
+
 
     let users = props.users.map(u => <User user={u} addFriend={props.addFriend} deleteFriend={props.deleteFriend} />);
 
     return (
         <div>
+            <button onClick={getUsers} className={style.uploadUsers}>Загрузить пользователей</button>
             {users}
         </div>
     );
