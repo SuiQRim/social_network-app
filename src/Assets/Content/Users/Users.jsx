45111982a285360e416ce5fc2bcb15e9d4f00c11
Roman Samadi <romikkimor123@gmail.com>
2022-11-16 08:25:33 +0300

    Переместил классовый комп. в контейнерную комп-ту

--- src/Assets/Content/Users/Users.jsx
+++ src/Assets/Content/Users/Users.jsx
@@ -1,47 +1,25 @@
-import axios from 'axios';
 import React from 'react';
 import TabNavigator from './TabNavigator/TabNavigator';
 import User from './User/User';
 import style from './Users.module.css'
 
-class Users extends React.Component {
+let Users = (props) => {
 
-    getUsers = (itemsInPage, selectedPage) => {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${itemsInPage}&page=${selectedPage}`).then(responce => {
-            this.props.setUsers(responce.data.items);
-            this.props.setTotalUserCount(responce.data.totalCount);
-        })
-    }
-
-    onPageChanged = (n) => {
-        this.props.setPage(n);
-        if(n < 1 ^ n > this.props.pagesCount) return;
-        this.getUsers(this.props.itemsInPageCount, n);
-    }
-
-    componentDidMount() {
-        if (this.props.users.length === 0) {
-            this.getUsers(this.props.itemsInPageCount, this.props.selectedPage);
-            console.log(this.props.selectedPage);
-        }
-    }
-
-    render() {
-        let users = this.props.users.map(u => <User key={u.id} user={u} addFriend={this.props.addFriend} deleteFriend={this.props.deleteFriend} />);
-        return (
-            <div>
-                <div className={style.tabNav}>
-                    <TabNavigator
-                        onPageChanged={this.onPageChanged} selectedPage={this.props.selectedPage} pagesCount={this.props.pagesCount}
-                        diapasoneStart={this.props.diapasoneStart} diapasone={this.props.diapasone} />
-                </div>
-                <div>
-                    {users}
-                </div>
-
+    let users = props.users.map(u => <User key={u.id} user={u} addFriend={props.addFriend} deleteFriend={props.deleteFriend} />);
+    return (
+        <div>
+            <div className={style.tabNav}>
+                <TabNavigator
+                    onPageChanged={props.onPageChanged} selectedPage={props.selectedPage} pagesCount={props.pagesCount}
+                    diapasoneStart={props.diapasoneStart} diapasone={props.diapasone} />
             </div>
-        );
-    }
+            <div>
+                {users}
+            </div>
+
+        </div>
+    );
+
 }
 
 
--- src/Assets/Content/Users/UsersContainer.jsx
+++ src/Assets/Content/Users/UsersContainer.jsx
@@ -1,6 +1,34 @@
+import axios from 'axios';
+import React from 'react';
 import { connect } from 'react-redux';
 import { addFriendAT, deleteFriendAT, setUsersAT,setTotalUserCountAC, setPageAC } from '../../../redux/users-reducer';
 import Users from './Users';
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            this.getUsers(this.props.itemsInPageCount, this.props.selectedPage);
+        }
+    }
+    getUsers = (itemsInPage, selectedPage) => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${itemsInPage}&page=${selectedPage}`).then(responce => {
+            this.props.setUsers(responce.data.items);
+            this.props.setTotalUserCount(responce.data.totalCount);
+        })
+    }
+
+    onPageChanged = (n) => {
+        this.props.setPage(n);
+        if(n < 1 ^ n > this.props.pagesCount) return;
+        this.getUsers(this.props.itemsInPageCount, n);
+    }
+    render(){
+        return <Users users={this.props.users} onPageChanged={this.onPageChanged} selectedPage={this.props.selectedPage} pagesCount={this.props.pagesCount}
+            diapasoneStart={this.props.diapasoneStart} diapasone={this.props.diapasone} addFriend={this.props.addFriend} deleteFriend={this.props.deleteFriend}/>;
+    };
+}
+
 
 const mapStateToProps = (state) => {
     return {
@@ -24,7 +52,6 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-let UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
 
 
-export default UsersContainer;
